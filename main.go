f84a7cdbd78a6f24779ecd87ddebc9e75fb8a60a
Clebson Moura <clebson.sh@gmail.com>
2025-08-31 16:45:55 -0300

    feat: read 8 bytes at time from file and print to stout

--- main.go
+++ main.go
@@ -1,7 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+)
 
-func main(){
-	fmt.Printf("I hope I get the job!")
+func main() {
+	fileHandler, err := os.Open("messages.txt")
+	if err != nil {
+		log.Fatal("Error", err)
+	}
+
+	for {
+		data := make([]byte, 8)
+
+		count, err := fileHandler.Read(data)
+		if err != nil {
+			break
+		}
+
+		fmt.Printf("read: %s\n", string(data[:count]))
+	}
 }
